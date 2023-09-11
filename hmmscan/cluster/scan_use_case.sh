e9c7096bc9ba083df531486befe902af60812067
josh-wilde <josh.t.wilde@gmail.com>
2023-09-11 09:04:54 -0400

    gitignore pycache

--- hmmscan/cluster/scan_use_case.sh
+++ hmmscan/cluster/scan_use_case.sh
@@ -3,8 +3,8 @@
 #SBATCH --mem-per-cpu=100M
 #SBATCH --partition=sched_mit_sloan_batch
 #SBATCH --time=0-00:30:00
-#SBATCH -o /home/jtwilde/projects/hmmscan/hmmscan/cluster/output.out
-#SBATCH -e /home/jtwilde/projects/hmmscan/hmmscan/cluster/error.err
+#SBATCH -o /home/jtwilde/projects/hmmscan-pytorch-private/hmmscan/cluster/output.out
+#SBATCH -e /home/jtwilde/projects/hmmscan-pytorch-private/hmmscan/cluster/error.err
 #SBATCH --mail-type=BEGIN,END,FAIL
 #SBATCH --mail-user=jtwilde@mit.edu
 
